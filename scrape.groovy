7c6400094f09414afed1899fd2984b8117a5ff87
David Dyer-Bennet <dd-b@dd-b.net>
2021-03-09 16:48:30 -0600

    Full config params, debug control, param logging

--- scrape.groovy
+++ scrape.groovy
@@ -1,5 +1,5 @@
 /*
-	Scraping Switch--present switch behavior controlled by web scraping.
+	"Scraping switch" for Hubitat Elevation
 
 	Copyright 2021 David Dyer-Bennet.
 
@@ -24,7 +24,11 @@
 	SOFTWARE.
 */
 
-static String version()	{  return '0.1.194'  }
+// This code lives in a repository at https://github.com/dd-b/HubitatScrapeBool
+// You can see change history there, I'm not going to try to keep it updated
+// in the file also.
+
+static String version()	{  return '0.1.195'  }
 
 metadata {
     definition (
@@ -42,8 +46,14 @@ metadata {
         input (name: "txtEnable", type: "bool", title: "Enable descriptionText logging",
 	       displayDuringSetup: true, defaultValue: true)
 	input (name: "scrapeUrl", type: "string", title: "URL to scrape",
+	       defaultValue: "http://example.com/API/snow-emergency/your-city.html",
 	       displayDuringSetup: true)
-// ZZDO Put conditionals around debug log statement using this
+	input (name: "pollSched", type: "string", title: "Polling schedule, crontab format",
+	       defaultValue: "47 16 4,6,8,12,14-19 * * *", // Change at least the seconds!
+	       displayDuringSetup: true)
+	input (name: "testRegexp", type: "string", title: "Regular expression to set switch on",
+	       defaultValue: "snow-emergency: yes",
+	       displayDuringSetup: true)
 	input (name: 'debugOn', type: 'bool', title: 'Enable debug logging?',
 	       displayDuringSetup: true, defaultValue: true)
     }
@@ -53,22 +63,29 @@ metadata {
 // when it's working. Maybe use fancy schedule for MPLS case? Know by 6pm,
 // don't check at all frequently when snow emergency already in effect?
 def initialize () {
-    log.debug "Starting polling";
     unschedule();		//Get rid of any existing schedule items
-    runEvery1Minute (pollUrl); // Note singular vs. plural!
+    log.info "Starting polling";
+    schedule (pollSched, pollUrl);
 }
 
+// Called when the device settings are saved
 void updated() {
-    log.info "Updated..."
-    log.warn "description logging is: ${txtEnable == true}"
+    log.info '''Updated device settings:
+    Description logging is: ${txtEnable == true}
+    Scrape URL: ${scrapeUrl}
+    Polling schedule: ${pollSched}
+    Test regexp: ${testRegexp}
+    Debug logging: ${debugOn}
+'''
     initialize()
 }
 
+// Called when the device is first created
 void installed() {
     log.info "Installed..."
     device.updateSetting("txtEnable",[type:"bool",value:true])
-    refresh()
-    initialize()
+//    refresh()
+//    initialize()
 }
 
 // Required by form, but not actually necessary if the device isn't recieving messages.
@@ -96,13 +113,16 @@ void off() {
     parent?.componentOff(this.device)
 }
 
+// Called when the refresh button on the device page is clicked
 void refresh() {
-    parent?.componentRefresh(this.device)
+    parent?.componentRefresh(this.device) // Can't happen?
+    // Force immediate poll, useful for testing
+    pollUrl()
 }
 
 // Start asynch poll of our scraping URL.
 void pollUrl() {
-    log.debug "Polling ${scrapeUrl} 30";
+    if (debugOn) log.debug "Polling ${scrapeUrl} 30";
     if (scrapeUrl) {
 	def params = [
             uri: 	scrapeUrl,
@@ -117,12 +137,12 @@ void pollUrl() {
 
 // Called when the async poll of the scraping URL completes.
 def procUrl (response, data) {
-    log.debug "Poll result ${response.status} returned data ${response.data}";
+    if (debugOn) log.debug "Poll result ${response.status} returned data ${response.data}";
     if (response.hasError()) {
         log.warn "response received error: ${response.getErrorMessage()}"
 	return
     }
-    if (response.data =~ /snow-emergency: yes/) {
+    if (response.data =~ testRegexp) {
 	sendEvent(name: 'on', value: 0, descriptionText: "Switch set on")
 	log.info "Switch set on";
     } else {
